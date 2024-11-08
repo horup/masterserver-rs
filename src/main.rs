9f369f29f8b069888070fbc75d95e74fd2465e60
horup <horup@deepstacker.com>
2024-11-08 12:49:28 +0100

    added try to env filter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tracing::info;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use tracing::{debug, info};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 mod matchbox;
 mod roomy;
@@ -7,6 +7,7 @@ mod roomy;
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")))
         .with(
             tracing_subscriber::fmt::layer()
                 .compact()
@@ -17,6 +18,7 @@ async fn main() {
     let roomy_port = 8080;
     let matchbox_port = 8081;
     info!("Starting Roomy on port {roomy_port} and Matchbox on port {matchbox_port}");
+    debug!("Debug enabled");
     let roomy_server = tokio::spawn(roomy::start(roomy_port));
     let matcbox_server = tokio::spawn(matchbox::start(matchbox_port));
     let _ = roomy_server.await;
